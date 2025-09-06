3b4265da28b54e3ef086eb601e5857889dae77a0
IDGS-901-22002224 <daniel1234593bdkdbdj@gmail.com>
2025-09-06 00:05:13 -0600

    Update RetrofitClient.kt

--- app/src/main/java/org/utl/huertovertical/api/RetrofitClient.kt
+++ app/src/main/java/org/utl/huertovertical/api/RetrofitClient.kt
@@ -6,7 +6,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-    private const val BASE_URL = "https://api.openweathermap.org/"
+    private const val BASE_URL = ""
 
     val retrofit: Retrofit by lazy {
         val logging = HttpLoggingInterceptor().apply {
