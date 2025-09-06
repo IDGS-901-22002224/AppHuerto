6ec454739d21bd0e10d39d74529be5bb6b434cd5
IDGS-901-22002224 <daniel1234593bdkdbdj@gmail.com>
2025-09-06 00:05:25 -0600

    Update WeatherApiService.kt

--- app/src/main/java/org/utl/huertovertical/api/WeatherApiService.kt
+++ app/src/main/java/org/utl/huertovertical/api/WeatherApiService.kt
@@ -6,7 +6,7 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface WeatherApiService {
-    @GET("data/2.5/forecast")
+    @GET("")
     suspend fun getFiveDayForecast(
         @Query("lat") lat: Double,
         @Query("lon") lon: Double,
